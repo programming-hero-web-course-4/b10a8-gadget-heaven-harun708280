8e057a52199e2122a55ef5461cbe30349c7ad066
harunbd80 <harun708280@gmail.com>
2024-11-06 23:00:44 +0600

    nav update

--- src/Component/Common/Nav.jsx
+++ src/Component/Common/Nav.jsx
@@ -11,62 +11,9 @@ const Nav = () => {
     return (
         <div className='w-11/12 mx-auto  '>
             {
-                pathname === '/about' || pathname === '/Statistics' || pathname === '/dashboard' ?
+                pathname === '/'  ?
                 
-                <div className="navbar rounded-t-lg  ">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                   
-                    </div>
-                   <Link to='/' className="  font-bold text-xl">Gadget Heaven  </Link>
-                </div>
-                <div className="navbar-center  hidden lg:flex">
-                    <ul className="menu menu-horizontal space-x-12  px-1">
-                        <NavLink to='/' className={({isActive})=>[
-                            isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
-                        ]}>Home</NavLink>
-                        <NavLink to='/about'className={({isActive})=>[
-                            isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
-                        ]}>About</NavLink>
-                        <NavLink to='Statistics' className={({isActive})=>[
-                            isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
-                        ]}>Statistics </NavLink>
-                        <NavLink to='/dashboard'className={({isActive})=>[
-                            isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
-                        ]}>Dashboard</NavLink>
-
-                    
-                    </ul>
-                </div>
-                <div className="navbar-end space-x-8 tex">
-                <div className="relative">
-                        <button className='h-10 w-10 bg-white rounded-full'><i class="fa-solid fa-cart-shopping"></i></button>
-                        <div className="absolute  -top-1 -right-1">
-                            <span>{card?.length}</span>
-                        </div>
-                    </div>
-                    <div className="relative">
-                        <button className='h-10 w-10  bg-white rounded-full'><i class="fa-regular fa-heart"></i></button>
-                        <div className="absolute  -top-1 -right-1">
-                            <span>{wishList?.length}</span>
-                        </div>
-                    </div>
-                </div>
-                </div>:
+                
                 <div className="navbar mt-7 rounded-t-lg  bg-common">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -122,6 +69,60 @@ const Nav = () => {
                     </div>
                     
                 </div>
+              </div>:
+              <div className="navbar rounded-t-lg  ">
+              <div className="navbar-start">
+                  <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                      <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4 6h16M4 12h8m-8 6h16" />
+                      </svg>
+                  </div>
+                 
+                  </div>
+                 <Link to='/' className="  font-bold text-xl">Gadget Heaven  </Link>
+              </div>
+              <div className="navbar-center  hidden lg:flex">
+                  <ul className="menu menu-horizontal space-x-12  px-1">
+                      <NavLink to='/' className={({isActive})=>[
+                          isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
+                      ]}>Home</NavLink>
+                      <NavLink to='/about'className={({isActive})=>[
+                          isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
+                      ]}>About</NavLink>
+                      <NavLink to='Statistics' className={({isActive})=>[
+                          isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
+                      ]}>Statistics </NavLink>
+                      <NavLink to='/dashboard'className={({isActive})=>[
+                          isActive?'border-b-4 border-common':'hover:border-b-2 hover:border-common'
+                      ]}>Dashboard</NavLink>
+
+                  
+                  </ul>
+              </div>
+              <div className="navbar-end space-x-8 tex">
+              <div className="relative">
+                      <button className='h-10 w-10 bg-white rounded-full'><i class="fa-solid fa-cart-shopping"></i></button>
+                      <div className="absolute  -top-1 -right-1">
+                          <span>{card?.length}</span>
+                      </div>
+                  </div>
+                  <div className="relative">
+                      <button className='h-10 w-10  bg-white rounded-full'><i class="fa-regular fa-heart"></i></button>
+                      <div className="absolute  -top-1 -right-1">
+                          <span>{wishList?.length}</span>
+                      </div>
+                  </div>
+              </div>
               </div>
             }
         </div>
